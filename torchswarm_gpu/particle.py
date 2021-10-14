Correct sign for gradient

torchswarm_gpu/particle.py
@@ -188,10 +188,10 @@ class HMCParticleWithGradients(Particle):
 class HMCParticle(HMCParticleWithGradients):
     def evaluate_grad(self):
         gbest_position = self.optimizer.gbest_position
-        return self.c1 * torch.rand(1) \
+        return -(self.c1 * torch.rand(1) \
                 * (self.mass_matrix @ (self.pbest_position - self.position)) \
                 + self.c2 * torch.rand(1) \
-                * (self.mass_matrix @ (gbest_position - self.position))
+                * (self.mass_matrix @ (gbest_position - self.position)))
 
     def set_ref_to_optimizer(self,optimizer):
         self.optimizer = optimizer